c6676530c3817fdc22410f0c6b378cba6711788e
mountainbug95 <smithtn2@goldmail.etsu.edu>
2019-04-10 13:36:55 -0400

    test 1

--- src/layouts/BetaSignUp.js
+++ src/layouts/BetaSignUp.js
@@ -7,12 +7,18 @@ class BetaSignUp extends Component {
             <div className="contact">
                 <section className="container">
                     <h1>Join the Beta Waitlist</h1>
-                    <form netlify netlify-honeypot="bot-field" name="beta" action="/thank-you" id="contact-form" role="form">
+                    <form data-netlify="true" netlify-honeypot="bot-field" name="beta" action="/thank-you" id="contact-form" role="form">
                         <input type="email" tabindex="1" name="_replyto" placeholder="Email Address" required/>
                         <button className="btn-primary knockout" tabindex="2" type="submit" name="submit">Sign Me Up</button>
                         <input type="hidden" className="hidden" name="bot-field"/>
                         <input type="hidden" name="form-name" value="beta"/>
                     </form>
+            
+            
+                    <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                        <input name="replyto" placeholder="Email Address" type="email" />
+                    </form>
+            
                 </section>
             </div>
         )
