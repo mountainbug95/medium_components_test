eb788f4ac503ecbfe0c7a8e1b665e73498c40f12
mountainbug95 <smithtn2@goldmail.etsu.edu>
2019-04-16 20:13:21 -0400

    test 9

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ResponsiveEmbed } from 'react-bootstrap';
 
 import '../../static/assets/css/blog-posts.scss';
 
@@ -6,10 +7,14 @@ class BlogPosts extends Component {
 
     singlePost(post) {
         return (
-            <div>
-                <div><img src={post.thumbnail}/></div>
-                <div>
-                    <div><a href={post.href}>{post.title}</a></div>
+            <div className="post-container">
+                <div className="thumbnail-container">
+                    <ResponsiveEmbed>
+                        <img src={ require('../../static/images/blogs/thumbnails/' + post.thumbnail + '.png') } className="thumbnail" />
+                    </ResponsiveEmbed>
+                </div>
+                <div className="post-info">
+                    <div><a href={post.href}><h1>{post.title}</h1></a></div>
                     <div>{post.excerpt}</div>
                 </div>
             </div>
